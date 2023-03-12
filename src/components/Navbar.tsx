a5b7205e148f2472b29e980668294835a70001ad
wolizhiquana <wolizhiquana@gmail.com>
2023-03-13 02:41:58 +0800

    Building game cards

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+
 import ColorModeSwitch from "./ColorModeSwitch";
+import logo from "../assets/logo.webp";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
